bc5691269ae0b523cdc30cdbe29043953fd4709e
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-03-21 00:44:53 -0400

    printing table using npm package console.table

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
+const mysql = require('mysql2');
 const express = require('express');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+/*---------------------------------------------------------------
+
+-                            CONNECT TO DB
+
+---------------------------------------------------------------*/
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'X9s0R0xpto#i7rzX8$*',
+    database: 'employee_tracker_db'
+  },
+  console.log('Connected to the election database.')
+);
 
 
+/*---------------------------------------------------------------
+-                       Express middleware
+---------------------------------------------------------------*/
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+/*---------------------------------------------------------------
+
+-                            PROMPTS
+
+---------------------------------------------------------------*/
 
 const promptHomeMenu = () => {
 
   return inquirer.prompt([
     {
       type: 'list',
-      name: 'Menu Options',
+      name: 'menu',
       message: 'What would you like to do?',
       choices: ['View All Departments', 
                 'View All Roles', 
@@ -19,17 +52,76 @@ const promptHomeMenu = () => {
                 'Update Employee Role']
     }
   ])
-  .then((answers) => {
-    console.log(answers);
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } 
-    else {
-      // Something else went wrong
+  .then(answers => { 
+    /*---------------------------------------------------------------
+    -                       OPTION 1 is Selected
+    ---------------------------------------------------------------*/
+    if (answers.menu === 'View All Departments') {
+      db.query(`SELECT * FROM department`, (err, rows) => {
+        console.table(rows);
+      });
+    }
+    /*---------------------------------------------------------------
+    -                       OPTION 2 is Selected
+    ---------------------------------------------------------------*/
+    else if(answers.menu === 'View All Roles') {
+
+      db.query(`SELECT * FROM jobrole`, (err, rows) => {
+        console.table(rows);
+      });
+    }
+    /*---------------------------------------------------------------
+    -                       OPTION 3 is Selected
+    ---------------------------------------------------------------*/
+    else if(answers.menu === 'View All Employees') { 
+      db.query(`SELECT * FROM employee`, (err, rows) => {
+        console.table(rows);
+      });
     }
   })
+    /*---------------------------------------------------------------
+    -                       Catch Error
+    ---------------------------------------------------------------*/
+  .catch((error) => {
+    if (error.isTtyError) {
+    } 
+  })
 };
+
+/*---------------------------------------------------------------
+
+-                            ROUTES
+
+---------------------------------------------------------------*/
+
+app.get('/api/department', (req, res) => {
+
+  const sql = `SELECT * FROM department`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+/*---------------------------------------------------------------
+
+-                            LISTENERS
+
+---------------------------------------------------------------*/
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 promptHomeMenu();
